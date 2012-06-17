d1abfafc40cfbb72c2b763ff858a8698f03d2558
wujin4991747 <wujin4931747@gmail.com>
2012-06-17 17:22:42 +0800

    addSinaWeiBo_NotDoneYet

---
+++ ILovepostcard/ShareAndBuyView/SinaWeiBoView/SinaShare/SinaShare.h
@@ -0,0 +1,13 @@
+//
+//  SinaShare.h
+//  ILovepostcard
+//
+//  Created by 进 吴 on 12-6-17.
+//  Copyright (c) 2012年 开趣. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SinaShare : NSObject
+
+@end
---
+++ ILovepostcard/ShareAndBuyView/SinaWeiBoView/SinaShare/SinaShare.m
@@ -0,0 +1,13 @@
+//
+//  SinaShare.m
+//  ILovepostcard
+//
+//  Created by 进 吴 on 12-6-17.
+//  Copyright (c) 2012年 开趣. All rights reserved.
+//
+
+#import "SinaShare.h"
+
+@implementation SinaShare
+
+@end
